f95be4439d4cef0bd50a31f6a507e38a77554cbf
YU-ZORA <234326330+Yuzora-Yu@users.noreply.github.com>
2026-08-26 16:08:08 +0900

    up

--- config.js
+++ config.js
@@ -1,10 +1,8 @@
 // URL BATTLER v0.3 frontend config
 window.URL_BATTLER_CONFIG = {
-  // Cloudflare Worker の /scan エンドポイント。
-  // 例: "https://url-battler-scan.example.workers.dev/scan"
-  scanEndpoint: "",
+  scanEndpoint:
+    "https://url-battler-scan.rikai-829.workers.dev/scan",
 
-  // SNS共有画像・投稿文に載せるURL BATTLER本体URL。
-  // 公開時に実URLへ変更してください。
-  publicAppUrl: "https://example.com/url-battler/"
+  publicAppUrl:
+    "http://localhost:8000/"
 };
